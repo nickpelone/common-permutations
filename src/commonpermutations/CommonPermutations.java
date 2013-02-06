c12487a09eacd82553c9328c6be81f9f84666f4b
Nick Pelone <npelone@unca.edu>
2013-02-06 16:53:46 -0500

    basic structure, beginning of for each loop to collect the letters in common

--- src/commonpermutations/CommonPermutations.java
+++ src/commonpermutations/CommonPermutations.java
@@ -4,6 +4,10 @@
  */
 package commonpermutations;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author nick
@@ -14,6 +18,42 @@ public class CommonPermutations {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        char[] a_array;
+        char[] b_array;
+        boolean go = true;
+        try{
+            //setup
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+            String a = inputReader.readLine();
+            if(a.length() == 0){
+                System.exit(0);
+            }
+            
+            String b = inputReader.readLine();
+            
+            while(go == true){
+                a_array = a.toCharArray();
+                b_array = b.toCharArray();
+                for(char x : a_array){
+                    
+                }
+                
+                    
+                }
+        
+                
+                
+                //handle a blank line, reset, move back to what we have to do
+            String finalTest = inputReader.readLine();
+            if(finalTest.length() == 0){
+                System.exit(0);
+            }else{
+               a = finalTest;
+               b = inputReader.readLine();
+            }
+            }
+        catch (IOException ioe){
+            
+        }
     }
 }
